51756b8016be5c36539a2243f72d2425a2460a46
Fang Yu Hsueh <fangop3@gmail.com>
2024-01-21 05:24:41 +0800

    Align examples codes with autopep8

--- table3.py
+++ table3.py
@@ -1,6 +1,6 @@
 from simplebigrad import Node
-Node.verbose =True
+Node.verbose = True
 x1 = Node(2)
 x2 = Node(5)
-y = Node.log(x1) + x1*x2 -Node.sin(x2)
+y = Node.log(x1) + x1*x2 - Node.sin(x2)
 y.backward()
