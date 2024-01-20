f90111c7391ae5a028ad69062de24c9ff9a55b7a
Fang Yu Hsueh <fangop3@gmail.com>
2024-01-21 04:32:06 +0800

    Align codes with documents

--- simplebigrad/simplebigrad.py
+++ simplebigrad/simplebigrad.py
@@ -3,101 +3,110 @@ from math import exp as math_exp
 from math import sin as math_sin
 from math import cos as math_cos
 
-class WrappedFloat:
+
+class Node:
     """A wrapped floating-point"""
     __wrapped_type = float
     verbose = False
-    def __init__(self, value:__wrapped_type, __parents=[], __op=None) -> None:
+
+    def __init__(self, value: __wrapped_type, parents=[], operator=None) -> None:
         self.value = value
-        self.__parents = __parents
-        self.__children = []
-        self.__op = __op
+        self.parents = parents
+        self.children = []
+        self.operator = operator
         self.grad = None
         self.grad_wrt = {}
         if self.verbose == True:
-            print('val:{:<10}|par:{:<30}|ops:{:<50}'.format(
+            print('Create Node:\n\tvalue:{:<10}\n\tparents:{:<30}\n\toperator:{:<50}'.format(
                 str(self.value.__round__(3)),
-                str([p.value.__round__(3) for p in self.__parents]),
-                self.__op.__repr__())
-                )
-    
+                str([p.value.__round__(3) for p in self.parents]),
+                self.operator.__repr__())
+            )
+
     def __repr__(self) -> str:
         if self.grad:
-            return f'WrappedFloat(value={self.value:.2f}, grad={self.grad:.3f})'
+            return f'Node(value={self.value:.2f}, grad={self.grad:.3f})'
         else:
-            return f'WrappedFloat(value={self.value:.2f}, grad=(Unsolved)'
+            return f'Node(value={self.value:.2f}, grad=(Unsolved)'
 
-    def add(x1,x2):
-        if not WrappedFloat.__instancecheck__(x1):
-            x1 = WrappedFloat(x1)
-        if not WrappedFloat.__instancecheck__(x2):
-            x2 = WrappedFloat(x2)
-        fnode = WrappedFloat(x1.value+x2.value, [x1,x2], WrappedFloat.__wrapped_type.__add__)
-        fnode.grad_wrt[x1] = 1
-        fnode.grad_wrt[x2] = 1
-        x1.__children.append(fnode)
-        x2.__children.append(fnode)
-        return fnode
+    def add(node1, node2):
+        if not Node.__instancecheck__(node1):
+            node1 = Node(node1)
+        if not Node.__instancecheck__(node2):
+            node2 = Node(node2)
+        childNode = Node(node1.value+node2.value,
+                         [node1, node2], Node.__wrapped_type.__add__)
+        childNode.grad_wrt[node1] = 1
+        childNode.grad_wrt[node2] = 1
+        node1.children.append(childNode)
+        node2.children.append(childNode)
+        return childNode
 
-    def sub(x1,x2):
-        if not WrappedFloat.__instancecheck__(x1):
-            x1 = WrappedFloat(x1)
-        if not WrappedFloat.__instancecheck__(x2):
-            x2 = WrappedFloat(x2)
-        fnode = WrappedFloat(x1.value-x2.value, [x1,x2], WrappedFloat.__wrapped_type.__sub__)
-        fnode.grad_wrt[x1] = 1
-        fnode.grad_wrt[x2] = -1
-        x1.__children.append(fnode)
-        x2.__children.append(fnode)
-        return fnode
+    def sub(node1, node2):
+        if not Node.__instancecheck__(node1):
+            node1 = Node(node1)
+        if not Node.__instancecheck__(node2):
+            node2 = Node(node2)
+        childNode = Node(node1.value-node2.value,
+                         [node1, node2], Node.__wrapped_type.__sub__)
+        childNode.grad_wrt[node1] = 1
+        childNode.grad_wrt[node2] = -1
+        node1.children.append(childNode)
+        node2.children.append(childNode)
+        return childNode
 
-    def mul(x1,x2):
-        if not WrappedFloat.__instancecheck__(x1):
-            x1 = WrappedFloat(x1)
-        if not WrappedFloat.__instancecheck__(x2):
-            x2 = WrappedFloat(x2)
-        fnode = WrappedFloat(x1.value * x2.value, [x1, x2], WrappedFloat.__wrapped_type.__mul__)
-        fnode.grad_wrt[x1] = x2.value
-        fnode.grad_wrt[x2] = x1.value
-        x1.__children.append(fnode)
-        x2.__children.append(fnode)
-        return fnode
+    def mul(node1, node2):
+        if not Node.__instancecheck__(node1):
+            node1 = Node(node1)
+        if not Node.__instancecheck__(node2):
+            node2 = Node(node2)
+        childNode = Node(node1.value * node2.value,
+                         [node1, node2], Node.__wrapped_type.__mul__)
+        childNode.grad_wrt[node1] = node2.value
+        childNode.grad_wrt[node2] = node1.value
+        node1.children.append(childNode)
+        node2.children.append(childNode)
+        return childNode
 
-    def pow(x1, x2):
-        if not WrappedFloat.__instancecheck__(x1):
-            x1 = WrappedFloat(x1)
-        if not WrappedFloat.__instancecheck__(x2):
-            x2 = WrappedFloat(x2)
-        fnode = WrappedFloat(x1.value ** x2.value, [x1,x2], WrappedFloat.__wrapped_type.__pow__)
-        fnode.grad_wrt[x1] = x2.value * x1.value**(x2.value - 1)
-        fnode.grad_wrt[x2] = (x1.value**x2.value)*math_log(x1.value)
-        x1.__children.append(fnode)
-        x2.__children.append(fnode)
-        return fnode
+    def pow(node1, node2):
+        if not Node.__instancecheck__(node1):
+            node1 = Node(node1)
+        if not Node.__instancecheck__(node2):
+            node2 = Node(node2)
+        childNode = Node(node1.value ** node2.value,
+                         [node1, node2], Node.__wrapped_type.__pow__)
+        childNode.grad_wrt[node1] = node2.value * \
+            node1.value**(node2.value - 1)
+        childNode.grad_wrt[node2] = (
+            node1.value**node2.value)*math_log(node1.value)
+        node1.children.append(childNode)
+        node2.children.append(childNode)
+        return childNode
 
-    def div(x1,x2):
-        if not WrappedFloat.__instancecheck__(x1):
-            x1 = WrappedFloat(x1)
-        if not WrappedFloat.__instancecheck__(x2):
-            x2 = WrappedFloat(x2)
-        fnode = WrappedFloat(x1.value / x2.value, [x1,x2], WrappedFloat.__wrapped_type.__truediv__)
-        fnode.grad_wrt[x1] = 1 / x1.value
-        fnode.grad_wrt[x2] = -x1.value / x2.value**2
-        x1.__children.append(fnode)
-        x2.__children.append(fnode)
-        return fnode
+    def div(node1, node2):
+        if not Node.__instancecheck__(node1):
+            node1 = Node(node1)
+        if not Node.__instancecheck__(node2):
+            node2 = Node(node2)
+        childNode = Node(node1.value / node2.value,
+                         [node1, node2], Node.__wrapped_type.__truediv__)
+        childNode.grad_wrt[node1] = 1 / node1.value
+        childNode.grad_wrt[node2] = -node1.value / node2.value**2
+        node1.children.append(childNode)
+        node2.children.append(childNode)
+        return childNode
 
-    def __add__(self,other):
+    def __add__(self, other):
         return self.add(other)
 
     def __radd__(self, other):
         return self.add(other)
 
-    def __sub__(self,other):
-        return WrappedFloat.sub(self,other)
+    def __sub__(self, other):
+        return Node.sub(self, other)
 
     def __rsub__(self, other):
-        return WrappedFloat.sub(other,self)
+        return Node.sub(other, self)
 
     def __mul__(self, other):
         return self.mul(other)
@@ -106,58 +115,61 @@ class WrappedFloat:
         return self.mul(other)
 
     def __truediv__(self, other):
-        return WrappedFloat.div(self,other)
-    
-    def __rtruediv__(self, other):
-        return WrappedFloat.div(other,self)
-  
-    def __pow__(self, other):
-        return WrappedFloat.pow(self,other)
+        return Node.div(self, other)
 
-    def __rpow__(self,other):
-        return WrappedFloat.pow(other,self)
+    def __rtruediv__(self, other):
+        return Node.div(other, self)
+
+    def __pow__(self, other):
+        return Node.pow(self, other)
+
+    def __rpow__(self, other):
+        return Node.pow(other, self)
 
     def __neg__(self):
         return self.mul(-1)
 
-    def log(x ,base=None):
-        if not WrappedFloat.__instancecheck__(x):
-            x = WrappedFloat(x)
+    def log(node1, base=None):
+        if not Node.__instancecheck__(node1):
+            node1 = Node(node1)
         if base == None:
-            base = WrappedFloat(math_exp(1))
-        elif not WrappedFloat.__instancecheck__(base):
-            base = WrappedFloat(base)
-        fnode = WrappedFloat(math_log(x.value,base.value), [x,base], math_log)
-        fnode.grad_wrt[x] = 1/(x.value*math_log(base.value))
-        fnode.grad_wrt[base] = -math_log(x.value)/(base.value*math_log(base.value**2))
-        x.__children.append(fnode)
-        base.__children.append(fnode)
-        return fnode
+            base = Node(math_exp(1))
+        elif not Node.__instancecheck__(base):
+            base = Node(base)
+        childNode = Node(math_log(node1.value, base.value),
+                         [node1, base], math_log)
+        childNode.grad_wrt[node1] = 1/(node1.value*math_log(base.value))
+        childNode.grad_wrt[base] = - \
+            math_log(node1.value)/(base.value*math_log(base.value**2))
+        node1.children.append(childNode)
+        base.children.append(childNode)
+        return childNode
 
-    def sin(x):
-        if not WrappedFloat.__instancecheck__(x):
-            x = WrappedFloat(x)
-        fnode = WrappedFloat(math_sin(x.value), [x], math_sin)
-        fnode.grad_wrt[x] = math_cos(x.value)
-        x.__children.append(fnode)
-        return fnode
+    def sin(node1):
+        if not Node.__instancecheck__(node1):
+            node1 = Node(node1)
+        childNode = Node(math_sin(node1.value), [node1], math_sin)
+        childNode.grad_wrt[node1] = math_cos(node1.value)
+        node1.children.append(childNode)
+        return childNode
 
-    def cos(x):
-        if not WrappedFloat.__instancecheck__(x):
-            x = WrappedFloat(x)
-        fnode = WrappedFloat(math_cos(x.value), [x], math_cos)
-        fnode.grad_wrt[x] = -math_sin(x.value)
-        x.__children.append(fnode)
-        return fnode
+    def cos(node1):
+        if not Node.__instancecheck__(node1):
+            node1 = Node(node1)
+        childNode = Node(math_cos(node1.value), [node1], math_cos)
+        childNode.grad_wrt[node1] = -math_sin(node1.value)
+        node1.children.append(childNode)
+        return childNode
 
     def forward(self):
         if self.verbose:
             print("Forward Tangent Trace:")
+
         def _topological_order():
             def _add_children(node):
                 if node not in visited:
                     visited.add(node)
-                    for child in node.__children:
+                    for child in node.children:
                         _add_children(child)
                     ordered.append(node)
 
@@ -166,8 +178,8 @@ class WrappedFloat:
             return ordered
 
         def _compute_grad_of_children(node):
-            for child in node.__children:
-                Δoutput_Δnode = node.grad 
+            for child in node.children:
+                Δoutput_Δnode = node.grad
                 Δchild_Δnode = child.grad_wrt[node]
                 if child.grad == None:
                     child.grad = Δoutput_Δnode * Δchild_Δnode
@@ -180,45 +192,19 @@ class WrappedFloat:
             if self.verbose == True:
                 print('val:{:<10}|par:{:<30}|grad:{:<30}'.format(
                     str(node.value.__round__(3)),
-                    str([p.value.__round__(3) for p in node.__parents]),
+                    str([p.value.__round__(3) for p in node.parents]),
                     str(node.grad.__round__(3)))
-                    )
-
-    def recursive_forward(self, q=None, __visisted=None):
-        if self.verbose:
-            print("Recursive Forward Tangent Trace:")
-        if __visisted == None and q==None:
-            __visisted = set()
-            __visisted.add(self)
-            q = deque()
-            self.grad = 1
-        for child in self.__children:
-            if child not in __visisted:
-                __visisted.add(child)
-                q.append(child)
-            Δoutput_Δnode = self.grad 
-            Δchild_Δnode = child.grad_wrt[self]
-            if child.grad == None:
-                child.grad = Δoutput_Δnode * Δchild_Δnode
-            else:
-                child.grad += Δoutput_Δnode * Δchild_Δnode
-            if self.verbose == True:
-                print('val:{:<10}|par:{:<30}|grad:{:<30}'.format(
-                    str(child.value.__round__(3)),
-                    str([p.value.__round__(3) for p in child.__parents]),
-                    str(child.grad.__round__(3)))
-                    )
-        if q:
-            q.popleft().recursive_forward(q,  __visisted)
+                )
 
     def backward(self):
         if self.verbose:
             print("Reverse Adjoint Trace:")
+
         def _topological_order():
             def _add_parents(node):
                 if node not in visited:
                     visited.add(node)
-                    for parent in node.__parents:
+                    for parent in node.parents:
                         _add_parents(parent)
                     ordered.append(node)
 
@@ -227,7 +213,7 @@ class WrappedFloat:
             return ordered
 
         def _compute_grad_of_parents(node):
-            for parent in node.__parents:
+            for parent in node.parents:
                 Δoutput_Δnode = node.grad
                 Δnode_Δparent = node.grad_wrt[parent]
 
@@ -243,6 +229,6 @@ class WrappedFloat:
             if self.verbose == True:
                 print('val:{:<10}|par:{:<30}|grad:{:<30}'.format(
                     str(node.value.__round__(3)),
-                    str([p.value.__round__(3) for p in node.__parents]),
+                    str([p.value.__round__(3) for p in node.parents]),
                     str(node.grad.__round__(3)))
-                    )
+                )
