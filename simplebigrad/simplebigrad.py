b7240469409ff535ee22917e9f8ea781250265f6
Fang Yu Hsueh <fangop3@gmail.com>
2024-01-23 01:38:19 +0800

    Align codes with document

--- simplebigrad/simplebigrad.py
+++ simplebigrad/simplebigrad.py
@@ -4,30 +4,21 @@ from math import sin as math_sin
 from math import cos as math_cos
 
 class Node:
-    """A wrapped floating-point"""
-    __wrapped_type = float
     verbose = False
 
-    def __init__(self, value: __wrapped_type, parents=[], operator='input') -> None:
+    def __init__(self, value, parent_nodes=[], operator='input',grad_wrt_parents=[]):
         self.value = value
-        self.parents = parents
-        self.children = []
+        self.parent_nodes = parent_nodes
         self.operator = operator
-        self.grad = None
-        self.grad_wrt = {}
+        self.grad_wrt_parents = grad_wrt_parents
+        self.child_nodes = []
+        self.grad = 0
         if self.verbose == True:
-            print('Create Node:\n\tvalue:{:<15}\n\tparents:{:<30}\n\toperator:{:<50}'.format(
+            print('Create a Node:\n\tvalue:{:<15}\n\tparents:{:<30}\n\toperator:{:<50}'.format(
                 str(self.value.__round__(3)),
-                str([p.value.__round__(3) for p in self.parents]),
+                str([p.value.__round__(3) for p in self.parent_nodes]),
                 self.operator.__repr__())
             )
-
-    def __repr__(self) -> str:
-        if self.grad:
-            return f'Node(value={self.value:.2f}, grad={self.grad:.3f})'
-        else:
-            return f'Node(value={self.value:.2f}, grad=(Unsolved)'
-
 
     def __add__(self, other):
         return add(self,other)
@@ -47,147 +38,77 @@ class Node:
     def __rmul__(self, other):
         return mul(other,self)
 
-    def __truediv__(self, other):
-        return div(self, other)
-
-    def __rtruediv__(self, other):
-        return div(other, self)
-
-    def __pow__(self, other):
-        return pow(self, other)
-
-    def __rpow__(self, other):
-        return pow(other, self)
-
-    def __neg__(self):
-        return mul(self,-1)
-
 def add(node1, node2):
-    if not Node.__instancecheck__(node1):
-        node1 = Node(node1)
-    if not Node.__instancecheck__(node2):
-        node2 = Node(node2)
-    childNode = Node(node1.value+node2.value,
-                        [node1, node2], 'add')
-    childNode.grad_wrt[node1] = 1
-    childNode.grad_wrt[node2] = 1
-    node1.children.append(childNode)
-    node2.children.append(childNode)
-    return childNode
+    value = node1.value + node2.value
+    parent_nodes = [node1,node2]
+    grad_wrt_parents = [1, 1]
+    newNode = Node(value, parent_nodes, "add", grad_wrt_parents)
+    node1.child_nodes.append(newNode)
+    node2.child_nodes.append(newNode)
+    return newNode
 
 def sub(node1, node2):
-    if not Node.__instancecheck__(node1):
-        node1 = Node(node1)
-    if not Node.__instancecheck__(node2):
-        node2 = Node(node2)
-    childNode = Node(node1.value-node2.value,
-                        [node1, node2], 'sub')
-    childNode.grad_wrt[node1] = 1
-    childNode.grad_wrt[node2] = -1
-    node1.children.append(childNode)
-    node2.children.append(childNode)
-    return childNode
+    value = node1.value - node2.value
+    parent_nodes = [node1,node2]
+    grad_wrt_parents = [1, -1]
+    newNode = Node(value, parent_nodes, "sub", grad_wrt_parents)
+    node1.child_nodes.append(newNode)
+    node2.child_nodes.append(newNode)
+    return newNode
 
 def mul(node1, node2):
-    if not Node.__instancecheck__(node1):
-        node1 = Node(node1)
-    if not Node.__instancecheck__(node2):
-        node2 = Node(node2)
-    childNode = Node(node1.value * node2.value,
-                        [node1, node2], 'mul')
-    childNode.grad_wrt[node1] = node2.value
-    childNode.grad_wrt[node2] = node1.value
-    node1.children.append(childNode)
-    node2.children.append(childNode)
-    return childNode
-
-def pow(node1, node2):
-    if not Node.__instancecheck__(node1):
-        node1 = Node(node1)
-    if not Node.__instancecheck__(node2):
-        node2 = Node(node2)
-    childNode = Node(node1.value ** node2.value,
-                        [node1, node2], 'pow')
-    childNode.grad_wrt[node1] = node2.value * \
-        node1.value**(node2.value - 1)
-    childNode.grad_wrt[node2] = (
-        node1.value**node2.value)*math_log(node1.value)
-    node1.children.append(childNode)
-    node2.children.append(childNode)
-    return childNode
-
-def div(node1, node2):
-    if not Node.__instancecheck__(node1):
-        node1 = Node(node1)
-    if not Node.__instancecheck__(node2):
-        node2 = Node(node2)
-    childNode = Node(node1.value / node2.value,
-                        [node1, node2], 'div')
-    childNode.grad_wrt[node1] = 1 / node1.value
-    childNode.grad_wrt[node2] = -node1.value / node2.value**2
-    node1.children.append(childNode)
-    node2.children.append(childNode)
-    return childNode
-
+    value = node1.value* node2.value
+    parent_nodes = [node1,node2]
+    grad_wrt_parents = [node2.value, node1.value]
+    newNode = Node(value, parent_nodes, "mul", grad_wrt_parents)
+    node1.child_nodes.append(newNode)
+    node2.child_nodes.append(newNode)
+    return newNode
 
 def log(node):
-    if not Node.__instancecheck__(node):
-        node = Node(node)
-    childNode = Node(math_log(node.value),
-                        [node], 'log')
-    childNode.grad_wrt[node] = 1/(node.value*math_log(math_exp(1)))
-    node.children.append(childNode)
-    return childNode
+    value = math_log(node.value)
+    parent_nodes = [node]
+    grad_wrt_parents = [1/(node.value*math_log(math_exp(1)))]
+    newNode = Node(value, parent_nodes, "log", grad_wrt_parents)
+    node.child_nodes.append(newNode)
+    return newNode
 
 def sin(node):
-    if not Node.__instancecheck__(node):
-        node1 = Node(node)
-    childNode = Node(math_sin(node.value), [node], 'sin')
-    childNode.grad_wrt[node] = math_cos(node.value)
-    node.children.append(childNode)
-    return childNode
-
-def cos(node):
-    if not Node.__instancecheck__(node):
-        node1 = Node(node)
-    childNode = Node(math_cos(node1.value), [node], 'cos')
-    childNode.grad_wrt[node] = -math_sin(node.value)
-    node.children.append(childNode)
-    return childNode
-
+    value = math_sin(node.value)
+    parent_nodes = [node]
+    grad_wrt_parents = [math_cos(node.value)]
+    newNode = Node(value, parent_nodes, "sin", grad_wrt_parents)
+    node.child_nodes.append(newNode)
+    return newNode
 
 def topological_order(rootNode):
     def add_children(node):
         if node not in visited:
             visited.add(node)
-            for child in node.children:
+            for child in node.child_nodes:
                 add_children(child)
-            ordered.append(node)
-
-    ordered, visited = [], set()
+            ordering.append(node)
+    ordering, visited = [], set()
     add_children(rootNode)
-    return reversed(ordered)
+    return reversed(ordering)
 
 def forward(rootNode):
     if rootNode.verbose:
-        print("Forward Tangent Trace:")
-
-    def _compute_grad_of_children(node):
-        for child in node.children:
-            Δoutput_Δnode = node.grad
-            Δchild_Δnode = child.grad_wrt[node]
-            if child.grad == None:
-                child.grad = Δoutput_Δnode * Δchild_Δnode
-            else:
-                child.grad += Δoutput_Δnode * Δchild_Δnode
+        print("Forward Mode:")
     rootNode.grad = 1
-    ordered = reversed(topological_order(rootNode))
-    for node in ordered:
-        _compute_grad_of_children(node)
+    ordering = topological_order(rootNode)
+    for node in ordering:
+        partial_derivative = 0
+        for i in range(len(node.parent_nodes)):
+            dnode_dparent = node.grad_wrt_parents[i]
+            dparent_droot = node.parent_nodes[i].grad
+            partial_derivative += dnode_dparent * dparent_droot
+            node.grad = partial_derivative
         if rootNode.verbose == True:
-            print('value:{:<15}|parents:{:<30}|gradient:{:<30}'.format(
+            print('value: {:<10}|parents: {:<20}|operator: {:<10}|gradient: {:<10}'.format(
                 str(node.value.__round__(3)),
-                str([p.value.__round__(3) for p in node.parents]),
+                str([p.value.__round__(3) for p in node.parent_nodes]),
+                str(node.operator),
                 str(node.grad.__round__(3)))
             )
 
@@ -195,36 +116,27 @@ def reverse_topological_order(rootNode):
     def add_parents(node):
         if node not in visited:
             visited.add(node)
-            for parent in node.parents:
-                add_parents(parent)
-            ordered.append(node)
-
-    ordered, visited = [], set()
+            for i in range(len(node.parent_nodes)):
+                add_parents(node.parent_nodes[i])
+            ordering.append(node)
+    ordering, visited = [], set()
     add_parents(rootNode)
-    return reversed(ordered)
-
+    return reversed(orderin)
 
 def backward(rootNode):
     if rootNode.verbose:
-        print("Reverse Adjoint Trace:")
-
-    def _compute_grad_of_parents(node):
-        for parent in node.parents:
-            Δoutput_Δnode = node.grad
-            Δnode_Δparent = node.grad_wrt[parent]
-
-            if parent.grad == None:
-                parent.grad = Δoutput_Δnode * Δnode_Δparent
-            else:
-                parent.grad += Δoutput_Δnode * Δnode_Δparent
-
+        print("Reverse Mode:")
     rootNode.grad = 1
-    ordered = reverse_topological_order(rootNode)
-    for node in ordered:
-        _compute_grad_of_parents(node)
+    ordering = reverse_topological_order(rootNode)
+    for node in ordering:
+        for i in range(len(node.parent_nodes)):
+            doutput_dnode = node.grad
+            dnode_dparent = node.grad_wrt_parents[i]
+            node.parent_nodes[i].grad += doutput_dnode * dnode_dparent
         if rootNode.verbose == True:
-            print('value:{:<15}|parents:{:<30}|gradient:{:<30}'.format(
+            print('value: {:<10}|parents: {:<20}|operator: {:<10}|gradient: {:<10}'.format(
                 str(node.value.__round__(3)),
-                str([p.value.__round__(3) for p in node.parents]),
+                str([p.value.__round__(3) for p in node.parent_nodes]),
+                str(node.operator),
                 str(node.grad.__round__(3)))
             )
