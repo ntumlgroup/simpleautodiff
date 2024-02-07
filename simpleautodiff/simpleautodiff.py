e1439dead3f1b7a9a90541a1756fbf867676b216
Fang Yu Hsueh <fangop3@gmail.com>
2024-02-07 20:55:41 +0800

    Format code for slides

--- simpleautodiff/simpleautodiff.py
+++ simpleautodiff/simpleautodiff.py
@@ -8,7 +8,7 @@ class Node:
     input_count = 0
     intermediate_count = 0
 
-    def __init__(self, value,parent_nodes=[],operator="input",):
+    def __init__(self, value, parent_nodes=[], operator="input",):
         self.value = value
         self.parent_nodes = parent_nodes
         self.child_nodes = []
@@ -34,7 +34,7 @@ class Node:
 def add(node1, node2):
     value = node1.value + node2.value
     parent_nodes = [node1, node2]
-    newNode = Node(value,parent_nodes,"add")
+    newNode = Node(value, parent_nodes, "add")
     newNode.grad_wrt_parents = [1, 1]
     node1.child_nodes.append(newNode)
     node2.child_nodes.append(newNode)
@@ -44,7 +44,7 @@ def add(node1, node2):
 def sub(node1, node2):
     value = node1.value - node2.value
     parent_nodes = [node1, node2]
-    newNode = Node(value,parent_nodes,"sub")
+    newNode = Node(value, parent_nodes, "sub")
     newNode.grad_wrt_parents = [1, -1]
     node1.child_nodes.append(newNode)
     node2.child_nodes.append(newNode)
@@ -54,8 +54,8 @@ def sub(node1, node2):
 def mul(node1, node2):
     value = node1.value * node2.value
     parent_nodes = [node1, node2]
-    newNode = Node(value,parent_nodes,"mul")
-    newNode.grad_wrt_parents = [node2.value, node1.value]
+    newNode = Node(value, parent_nodes, "mul")
+    newNode.grad_wrt_parents = [node2.value,node1.value]
     node1.child_nodes.append(newNode)
     node2.child_nodes.append(newNode)
     return newNode
@@ -64,7 +64,7 @@ def mul(node1, node2):
 def log(node):
     value = math_log(node.value)
     parent_nodes = [node]
-    newNode = Node(value,parent_nodes,"log")
+    newNode = Node(value, parent_nodes, "log")
     newNode.grad_wrt_parents = [1/(node.value)]
     node.child_nodes.append(newNode)
     return newNode
@@ -73,7 +73,7 @@ def log(node):
 def sin(node):
     value = math_sin(node.value)
     parent_nodes = [node]
-    newNode = Node(value,parent_nodes,"sin")
+    newNode = Node(value, parent_nodes, "sin")
     newNode.grad_wrt_parents = [math_cos(node.value)]
     node.child_nodes.append(newNode)
     return newNode
@@ -109,7 +109,8 @@ def forward(rootNode):
                 symbol_process += "(d" + node.name + "/d" + node.parent_nodes[i].name + ")"\
                                   + "(d" + node.parent_nodes[i].name + "/d" + rootNode.name + ") + "
                 value_process += "(" + str(dnode_dparent.__round__(3)) + ")(" + \
-                    str(node.parent_nodes[i].partial_derivative.__round__(3)) + ") + "
+                    str(node.parent_nodes[i].partial_derivative.__round__(
+                        3)) + ") + "
             print('d{:<2}/d{:<2} = {:<45} \n\t= {:<30} = {:<5}'.format(
                 node.name,
                 rootNode.name,
